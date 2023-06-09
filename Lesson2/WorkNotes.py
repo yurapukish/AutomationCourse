1e822dfbbfae3d0a13635b12f177f59795526b06
Y.Pukish <yura.pukish@vibodj.com>
2023-06-09 19:50:26 +0300

    Play with git, should leave only hometask into PR

--- Lesson2/WorkNotes.py
+++ Lesson2/WorkNotes.py
@@ -3,7 +3,7 @@ That floats on high o'er vales and hills,
 When all at once I saw a crowd,
 A host, of golden daffodils;
 Beside the lake, beneath the trees,
-Fluttering and dancing in the breeze. 
+Fluttering and dancing in the breeze.
 
 Continuous as the stars that shine
 And twinkle on the Milky Way,
@@ -14,24 +14,25 @@ Tossing their heads in sprightly dance."""
 
 # task 1
 """
-Logic: 
+Logic:
 1) Create a list of vowels
 2) Iterate over the text and count each vowel
 3) Print the result
 """
 vowels = ('a', 'e', 'i', 'o', 'u')
 table_style = (32, 12)
-print("-" * table_style[0])
+print('-' * table_style[0])
 print(f"| {'vowel':^{table_style[1]}} |  {'count':^{table_style[1]}} |")
-print("-" * table_style[0])
+print('-' * table_style[0])
 for vowel in vowels:
-    print(f"| {vowel:^{table_style[1]}} -> {poem_text.lower().count(vowel):^{table_style[1]}} |")
-print("-" * table_style[0])
+    print((f'| {vowel:^{table_style[1]}} -> '
+          f'{poem_text.lower().count(vowel):^{table_style[1]}} |'))
+print('-' * table_style[0])
 
 
 # task 2
 """
-Logic: 
+Logic:
 1) Create a function with a parameter
 2) Iterate over the text and replace each vowel
 3) Return the result
@@ -39,6 +40,15 @@ Logic:
 
 
 def replace_symbols(text):
+    """
+    Replace vowels in the text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The modified text with changed vowels.
+    """
     symbols_be_replaced = {'A': 'À', 'a': 'à', 'E': 'É', 'e': 'é', 'I': 'Í',
                            'i': 'í', 'O': 'Ó', 'o': 'ó', 'U': 'Ú', 'u': 'ú'}
 
