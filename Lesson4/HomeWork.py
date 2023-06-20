78bfed5fac0ad7daea52bc27616cf43b753ce0f0
yurapukish <64479097+yurapukish@users.noreply.github.com>
2023-06-20 18:43:07 +0300

    Update HomeWork.py

--- Lesson4/HomeWork.py
+++ Lesson4/HomeWork.py
@@ -3,7 +3,7 @@ Homework 3.
 
 We have four values w,x,y,z
 write if-elif-else statement that will search minimum value and print
-smth aka "'y' is minimum value'"
+smth aka "'y' is minimum value"
 
 """
 x, y, z, w = 100, 0, 32, 123
