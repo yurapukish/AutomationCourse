1faa2a6ffde5bc38e8a9db1afb6c6f601d8fa814
yurapukish <64479097+yurapukish@users.noreply.github.com>
2023-07-02 14:39:36 +0300

    Update HomeWork.py

--- Lessons8/HomeWork.py
+++ Lessons8/HomeWork.py
@@ -1,14 +1,14 @@
 """
 HomeWork lesson 8.
 
-Given the list of integers ( positive , negative, zeros)
+Given the list of integers ( positive, negative, zeros)
 Find max multiplication of three values in list
 
 Input: [-1, 1, 2, 0, 3, 12, 4, 5, -1, 6]
 Output: Max value = "360". Nums are: (12, 5, 6)
 
 My solution:
-1) Sort the list by absolute value of each element
+1) Sort the list by the absolute value of each element
 2) Create positive and negative lists
 3) Append values to positive and negative lists
 4) Find the max value and save elements in output
@@ -18,8 +18,6 @@ l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Sort the list by absolute value of each element
 l1.sort(key=lambda x: abs(x), reverse=True)
 
-# Create positive and negative lists
-positive, negative = [], []
 
 # Separate positive and negative elements
 positive, negative = [el for el in l1 if el > 0], [el for el in l1 if el <= 0]
@@ -27,7 +25,7 @@ positive_len, negative_len = len(positive), len(negative)
 
 if 1 <= positive_len < 3:
     # if positive elements are 1 or 2
-    # we multiply 1 positive element and 2 negative elements
+    # We multiply 1 positive element and 2 negative elements
     result = positive[0] * negative[0] * negative[1]
     output = positive[0], negative[0], negative[1]
 
@@ -45,8 +43,8 @@ elif positive_len >= 3:
             result = result2
             output = output2
 else:
-    # if no positive elements
-    # we multiply all negative elements
+    # If no positive elements
+    # We multiply all negative elements
     result = negative[-1] * negative[-2] * negative[-3]
     output = negative[-1], negative[-2], negative[-3]
 
