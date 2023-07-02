caa8ec7d262ef6d5b32e5d800014c9ed5753f52b
yurapukish <64479097+yurapukish@users.noreply.github.com>
2023-07-02 14:35:16 +0300

    Lesson8 (#10)
    * Done Homework 6
    
    * Finish Homework 7
    
    * revert
    
    * Add homework7
    
    * HomeWork8 Find max multiplication of three values in list
    
    ---------
    
    Co-authored-by: Y.Pukish <yura.pukish@vibodj.com>

---
+++ Lessons8/HomeWork.py
@@ -0,0 +1,54 @@
+"""
+HomeWork lesson 8.
+
+Given the list of integers ( positive , negative, zeros)
+Find max multiplication of three values in list
+
+Input: [-1, 1, 2, 0, 3, 12, 4, 5, -1, 6]
+Output: Max value = "360". Nums are: (12, 5, 6)
+
+My solution:
+1) Sort the list by absolute value of each element
+2) Create positive and negative lists
+3) Append values to positive and negative lists
+4) Find the max value and save elements in output
+"""
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Sort the list by absolute value of each element
+l1.sort(key=lambda x: abs(x), reverse=True)
+
+# Create positive and negative lists
+positive, negative = [], []
+
+# Separate positive and negative elements
+positive, negative = [el for el in l1 if el > 0], [el for el in l1 if el <= 0]
+positive_len, negative_len = len(positive), len(negative)
+
+if 1 <= positive_len < 3:
+    # if positive elements are 1 or 2
+    # we multiply 1 positive element and 2 negative elements
+    result = positive[0] * negative[0] * negative[1]
+    output = positive[0], negative[0], negative[1]
+
+elif positive_len >= 3:
+    # if positive elements are 3 or more
+    # we have 2 options: *all positive
+    #                    negative elements  * positive element
+    result = positive[0] * positive[1] * positive[2]
+    output = (positive[0], positive[1], positive[2])
+    if negative_len > 1:
+        # multiply all negative elements
+        result2 = positive[0] * negative[0] * negative[1]
+        output2 = positive[0], negative[0], negative[1]
+        if result2 > result:
+            result = result2
+            output = output2
+else:
+    # if no positive elements
+    # we multiply all negative elements
+    result = negative[-1] * negative[-2] * negative[-3]
+    output = negative[-1], negative[-2], negative[-3]
+
+# Print results
+print(f'Max value = "{result}". Nums are: {output}')
