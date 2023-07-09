2e7ee4102da277b45facb718a84118313ccc0240
yurapukish <64479097+yurapukish@users.noreply.github.com>
2023-07-09 23:56:57 +0300

    HomeWork 9 (#12)
    * HomeWork 9
    
    * Update HomeWork.py
    
    Change findall to finditer
    
    ---------
    
    Co-authored-by: Y.Pukish <yura.pukish@vibodj.com>

---
+++ Lessons9/HomeWork.py
@@ -0,0 +1,39 @@
+"""
+HomeWork 9.
+
+Write script that will read "input.txt" file and find there
+all characters from english alphabet collect these characters
+and their positions in file after info collected this info
+as text write to "output.txt" file in such format.
+####################
+<first found letter> -> pos1
+<next_letter> -> pos2
+<next_letter -> pos3
+etc
+#######################
+"""
+
+import re
+
+try:
+    # Create file to write the output
+    with open('output.txt', 'w') as output_file:
+        # Add a header to the output file
+        output_file.write(f'{"#" * 20}\n')
+
+        # Step 1: Open the input.txt file and read it
+        with open('input.txt', 'r') as input_file:
+            text = input_file.read()
+
+            # Step 2: Find all matches using a regular expression
+            # Update here using finditer
+            matches = re.finditer(r'[a-zA-Z]', text)
+        # Step 3: Write the matches and their indices to the output file
+        output_file.writelines(f'{el.group():^7} -> {el.start():^7}\n'
+                               for el in matches)
+        # Add footer to the output file
+        output_file.write(f'{"#" * 20}\n')
+
+except Exception as g_exc:
+    # Print error if occurs
+    print(g_exc)
